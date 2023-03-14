b21f286895588d98137536570d801803f127631f
Nick Taylor <nick@iamdeveloper.com>
2023-03-14 19:16:35 -0400

    test: added a test to add an item to the soon list

--- cypress/e2e/smoke-test.cy.js
+++ cypress/e2e/smoke-test.cy.js
@@ -1,5 +1,11 @@
-describe('Smoke Test', () => {
+describe('Add an item to the soon list', () => {
   it('should be able to visit the app', () => {
     cy.visit('/');
+    cy.findByRole('heading', { name: /smart shopper/i }).should('exist');
+    cy.findByRole('button', { name: /get started/i }).click();
+    cy.findByRole('img', { name: /loading/i }).should('not.exist');
+    cy.findByRole('link', { name: /start your list/i }).click();
+    cy.findByRole('textbox', { name: /item name:/i }).type('milk');
+    cy.findByRole('button', { name: /add item/i }).click();
   });
 });
